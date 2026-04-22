eaa8e15bde27a4b22081558b2af7f77c075f75dc
ayush5735 <ayush100074@gmail.com>
2026-04-22 22:11:45 +0000

    variable and constant

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,19 @@
+const accountId = 14453
+let accountEmail="helllo"
+var accountPassword="1234"
+accountCity="jaipur"
+
+// accountId=2.     // not allowed
+
+accountEmail="hehehe"
+accountPassword="6738393"
+accountCity="blr"
+
+console.log(accountId);
+
+
+/*
+prefer not to use war because of issue in block scope and functional scope
+*/
+
+console.table([accountId, accountEmail, accountPassword, accountCity])
