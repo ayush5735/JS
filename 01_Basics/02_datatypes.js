8cdd1742ff3c676b6995b18468f18cda09b5e09b
ayush5735 <ayush100074@gmail.com>
2026-04-23 02:49:51 +0000

    datatypes

---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,28 @@
+"use strict"; //  treat all JS codr as newer version
+
+// alert(3+3).  // we are using nodejs, not browser
+
+
+let name ="ayush"
+let age = 65
+let isLoggedIn = false
+let state;
+
+// number => 2 to pwer 53
+// bigint
+// string => ""
+// boolean => true, false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+
+// object
+
+console.log(typeof"ayush");      // string
+
+
+console.log(typeof null).      // object
+
+console.log(typeof undefined)   //undefined
