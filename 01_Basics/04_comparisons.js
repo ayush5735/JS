4af6990ded31951c99f784d88373d8e2370a4483
ayush5735 <ayush100074@gmail.com>
2026-04-24 21:04:19 +0000

    comparison

---
+++ 01_Basics/04_comparisons.js
@@ -0,0 +1,14 @@
+// console.log("2">1)   // dont compare two different datatypes
+
+// console.log(null > 0)
+// console.log(null == 0)
+// console.log(null >= 0)       // comparison convert null to a number it as 0 that's why it is true
+
+console.log(undefined==0)
+console.log(undefined>0)
+console.log(undefined<0)
+console.log(undefined>=0)         // avoid this type comparison since it is confusing
+
+// ===
+
+// console.log("2"===2)
