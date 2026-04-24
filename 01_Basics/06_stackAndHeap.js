42ff0949f523b491be7275b7547b499ce172aaf3
ayush5735 <ayush100074@gmail.com>
2026-04-24 21:35:31 +0000

    stack and heap

---
+++ 01_Basics/06_stackAndHeap.js
@@ -0,0 +1,25 @@
+// Stack(primitive), Heap(non primitive)
+
+
+let myYoutubename = "ayushhello";
+
+let anothername = myYoutubename;
+
+anothername = " hello you"
+
+console.log(myYoutubename)
+
+console.log(anothername);
+
+let userOne = {
+    email: "ayush@hh.com",
+    upi : "user@ybl"
+}
+
+let userTwo = userOne
+
+userTwo.email = "ayushlogout"
+
+console.log(userOne.email)
+console.log(userTwo.email)
+
