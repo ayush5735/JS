9dbe8136d74e1ff32f0b92bb98c82d4cf7827955
ayush5735 <ayush100074@gmail.com>
2026-04-24 21:21:34 +0000

    datatype summary

---
+++ 01_Basics/05_summaryDatatypes.js
@@ -0,0 +1,26 @@
+// Primitive
+
+// 7 : String, Number, boolean, null, undefined, Symbol, BigInt
+
+// javascript is dynamicallyTyped
+
+// Reference type (non primitive)
+// Array, Objects, Functions
+
+const heros=["ayush","ram","shyam"]
+console.log(heros)
+let myObj = {
+    name: "ayush",
+    age: 22,
+
+
+}
+
+const myFunction = function(){
+    console.log("hello you");
+    
+}
+
+console.log(typeof heros)
+
+// datypr of null if it is checked from typeof then it comes out as object
