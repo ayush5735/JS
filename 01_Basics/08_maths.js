ea4c3ec31838ba2e3724d37d6767d04f43577772
ayush5735 <ayush100074@gmail.com>
2026-04-27 18:08:58 +0000

    maths

---
+++ 01_Basics/08_maths.js
@@ -0,0 +1,47 @@
+// const score = 400
+// console.log(score)
+
+// const balance = new Number(100)
+
+// console.log(balance)
+
+// console.log(balance.toString().length)
+
+// console.log(balance.toFixed(2))
+
+// const otherNumber = 123.8998
+
+// console.log(otherNumber.toPrecision(3))
+
+// const hundreds = 10000000000
+
+// console.log(hundreds.toLocaleString('en-IN'))        // en-in is used to seperate zero in commas in indian orders
+
+
+//      maths //
+
+// console.log(Math)
+
+// console.log(Math.abs(4))
+
+// console.log(Math.round(4.6))
+
+// console.log(Math.ceil(4.2))
+
+// console.log(Math.floor(4.9))
+
+// console.log(Math.min(3,5,7,3,2,7,9))
+
+console.log(Math.random())
+
+console.log((Math.random()*10)+1)
+
+console.log(Math.floor(Math.random()*10)+1)
+
+
+const min = 10
+const max = 20
+// console.log(Math.random() * (max-min+1))      //formula
+
+console.log(Math.floor(Math.random() * (max-min+1)) + min)
+
